6b03d62e9e9c17046c18a2c4ed0340d48eef7d2c
Patrick Suclla <sucllaerazop@gmail.com>
2024-07-01 04:14:48 -0500

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
+  app.enableCors();
+
   app.setGlobalPrefix('api/v1')
   app.useGlobalPipes(
     new ValidationPipe({
